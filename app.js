1d72267f9f38739a907695415c7bfc0bc127bcf2
Sake Maneesh <maneeshsake99@gmail.com>
2021-11-19 11:19:40 -0600

    adding a file

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var tea = require("./models/tea"); 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var tea = require("./models/tea");
 
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -16,37 +18,37 @@ mongoose.connect(connectionString, {
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
+db.once("open", function () {
   console.log("Connection to DB succeeded")
 });
 
 // We can seed the collection if needed on server start 
-async function recreateDB(){ 
+async function recreateDB() {
   // Delete everything 
-  await tea.deleteMany(); 
- 
-  let instance1 = new tea({tea_type:"black tea",  size:'extra large', cost:37});
-  let instance2 = new tea({tea_type:"green tea",  size:'large', cost:26});
-  let instance3 = new tea({tea_type:"white tea",  size:'large', cost:34});
+  await tea.deleteMany();
 
-  instance1.save( function(err,doc) { 
-      if(err) return console.error(err); 
-      console.log("First object saved") 
-  }); 
+  let instance1 = new tea({ tea_type: "black tea", size: 'extra large', cost: 37 });
+  let instance2 = new tea({ tea_type: "green tea", size: 'large', cost: 26 });
+  let instance3 = new tea({ tea_type: "white tea", size: 'large', cost: 34 });
 
-  instance2.save( function(err,doc) { 
-    if(err) return console.error(err); 
-    console.log("Second object saved") 
-}); 
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
 
-  instance3.save( function(err,doc) { 
-    if(err) return console.error(err); 
-    console.log("Third object saved") 
-}); 
-} 
- 
-let reseed = true; 
-if (reseed) { recreateDB();} 
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
+  });
+
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved")
+  });
+}
+
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -61,7 +63,32 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }))
 
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -77,12 +104,12 @@ app.use('/resource', resourceRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
